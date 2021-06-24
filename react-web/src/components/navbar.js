bc7d24144ccdd096e9d62019e898468f5e06fd20
Jishnudeep Bhattacharjee <bjishnudeep@gmail.com>
2021-06-24 21:16:33 +0530

    Hero Section Completed

--- react-web/src/components/navbar.js
+++ react-web/src/components/navbar.js
@@ -31,8 +31,8 @@ function Navbar() {
       <nav className='navbar'>
         <div className='navbar-container'>
           <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-            TRVL
-            <i class='fab fa-typo3' />
+            M/\P
+              <i class="fas fa-map-marked-alt"></i>
           </Link>
           <div className='menu-icon' onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
