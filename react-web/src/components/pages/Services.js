a7f5d35e4179dbd9bfb9a973164baa322fa3bd9e
Jishnudeep Bhattacharjee <bjishnudeep@gmail.com>
2021-06-25 11:29:46 +0530

    added footer and fixed media

--- react-web/src/components/pages/Services.js
+++ react-web/src/components/pages/Services.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import '../../App.css'
+import { useEffect } from 'react';
+
 
 export default function Services(){
+    useEffect(() => {
+        window.scrollTo(0, 0)
+      }, [])
     return <h1 className = 'services'>SERVICES </h1>;
 }
